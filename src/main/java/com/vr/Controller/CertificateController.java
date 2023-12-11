7349a365661d2637ebbef2dc0283b65facc69d98
worntjd142 <149766907+worntjd142@users.noreply.github.com>
2023-12-11 16:09:34 +0900

    23/12/11 증명서 상세 내역 중간

--- src/main/java/com/vr/Controller/CertificateController.java
+++ src/main/java/com/vr/Controller/CertificateController.java
@@ -6,9 +6,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class CertificateController {
 
+	
+	//재증명 발급 화면으로 이동
 	@GetMapping("Certificate_L")
 	public String Certificate() {
 		return "Certificate/Certificate_L";
 	}
 	
+	//진료 기록 확인하기
+	@GetMapping("Serch")
+	public String serch() {
+	 return "Certificate/Certificate_L";
+	}
+	
+	//재증명 발급 상세내역
+	@GetMapping("CertificateDetails_L")
+	public String CertificateDetails() {
+		return "Certificate/CertificateDetails_L";
+	}
 }
