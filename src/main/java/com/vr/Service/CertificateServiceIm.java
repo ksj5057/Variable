ed319341c79278f8be556d07b77333deaec04ac2
worntjd142 <149766907+worntjd142@users.noreply.github.com>
2023-12-14 15:13:19 +0900

    23/12/14 재증명 이동 시 조건 추가

---
+++ src/main/java/com/vr/Service/CertificateServiceIm.java
@@ -0,0 +1,19 @@
+package com.vr.Service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.vr.Mapper.CertificateListMapper;
+import com.vr.Model.CertificateListDTO;
+import com.vr.Model.MemberDTO;
+
+@Service
+public class CertificateServiceIm implements CertificateService {
+
+	@Autowired
+	CertificateListMapper cm;
+	
+	public int serch(MemberDTO md) {
+		return cm.serch(md);
+	};
+}
