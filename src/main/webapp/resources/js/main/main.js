92e9a904cb5d6b33909101c0992910cdd7c72369
GR <GR@GRN-304-05>
2023-12-19 10:10:07 +0900

    [2023-12-19] 상단메뉴 증명서발급 로그인되면 확인서 목록 출력 추가

--- src/main/webapp/resources/js/main/main.js
+++ src/main/webapp/resources/js/main/main.js
@@ -1,21 +1,28 @@
 /**
  * 메인 메뉴 애니메이션
+ *
+ * alert('zz');
  */
 
-//문서가 로딩이 다 되면
-/*$(function(){
-	//menu_btn_mask옆에 버튼을 누르면
-	$('.menu_btn_mask > .open_menu_btn').click(function(){
-		//옆에 ul태그를 토글 !
-		$(this).next(".snb").toggleClass("hide");
+// 로그인했을때
+$(function(){
+	// 증명서에 마우스 올리면
+	$(".sbs_ul2").mouseover(function(){
+		// 확인서목록 슬라이드
+		$(".sbs__ul3").stop().slideDown(200);
+	});
+	// 마우스떼면 확인서목록 닫힘슬라이드
+	$(".sbs_ul2").mouseleave(function(){
+		$(".sbs__ul3").stop().slideUp(200);
 	});
 });
-*/
+
+// 상단메뉴 눌렀을때
 $(function(){
 	//menu_btn_mask옆에 버튼을 누르면
 	$('.menu_btn_mask > .open_menu_btn').click(function(){
 		//옆에 ul태그를 토글 !
-		$(this).next(".bh_snb_ul").toggle("ul");
+		$(this).next(".bh_snb_ul").toggle("slow");
 	});
 });
 
