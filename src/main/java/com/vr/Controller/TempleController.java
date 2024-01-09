d7cf2915029e041749629427dc14f608a8e5b6a3
GR <GR@GRN-304-05>
2024-01-09 10:40:52 +0900

    1040

--- src/main/java/com/vr/Controller/TempleController.java
+++ src/main/java/com/vr/Controller/TempleController.java
@@ -1,11 +1,18 @@
 package com.vr.Controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.vr.Model.MemberDTO;
 import com.vr.Model.TempleDTO;
 import com.vr.Service.TempleService;
 
@@ -14,7 +21,7 @@ public class TempleController {
 
 	@Autowired
 	TempleService ts;
-	
+
 	@RequestMapping(value = "test", method = RequestMethod.GET)
 	public String temple(TempleDTO td, Model model) {
 		// select 값을 tmps에 저장
@@ -22,5 +29,10 @@ public class TempleController {
 		return "biometric/test";
 	}
 
-	
+	@RequestMapping(value = "/test.action", method = RequestMethod.GET)
+	public ResponseEntity<?> temple(TempleDTO td, Model model, HttpSession session) {
+
+		return new ResponseEntity<>(ts.temp_s(td), HttpStatus.OK);
+	}
+
 }
--- src/main/webapp/WEB-INF/views/biometric/test.jsp
+++ src/main/webapp/WEB-INF/views/biometric/test.jsp
@@ -5,21 +5,31 @@
 <!DOCTYPE html>
 <html>
 <head>
+<script
+	src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.min.js"></script>
+
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/html2canvas/0.5.0-beta4/html2canvas.min.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.5.3/jspdf.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+
+<script type="text/javascript" src="/resources/js/biometric/test.js"></script>
 
 <meta charset="UTF-8">
 <title>Insert title here</title>
 </head>
 <body>
 	<h1>온도</h1>
-	
-	<!-- 
-		컨트롤에 저장한 temple을 items 으로 표현 
-		var="temple"에 저장 후 뿌리기?
-	-->
-	<c:forEach items="${temple}" var="temple" varStatus="status">
-	<ul>
-	<li><c:out value="${status.0}"></c:out></li>
+<%-- 	<!-- 컨트롤에 저장한 temple을 items 으로 표현 
+		var="temple"에 저장 후 뿌리기? -->
+	<c:forEach items="${temple}" var="temple" begin="0" end="0" >
+	<!-- temple 인덱스 0부터 0까지 반복문 li에 출력-->
+	<ul id="templelist">
+	<li>${temple.temp}</li>
 	</ul>
-	</c:forEach>
+	</c:forEach> --%>
+	
+	<canvas id="line-chart" width="400" height="400"></canvas>
+	
+
 </body>
 </html>
