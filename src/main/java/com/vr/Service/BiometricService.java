e055e4fbe74036ba25f5c46fc2cd979782846704
GR <GR@GRN-304-05>
2024-01-09 12:03:52 +0900

    복붙의 신

--- src/main/java/com/vr/Service/BiometricService.java
+++ src/main/java/com/vr/Service/BiometricService.java
@@ -14,7 +14,11 @@ public interface BiometricService {
 	public BiometricDTO babydetail(BiometricDTO bd);
 	
 	//301~303 db의  리스트
-	public ArrayList<BiometricDTO> babylist(BiometricDTO bd);
+	public ArrayList<BiometricDTO> babylist1(BiometricDTO bd);
+	
+	public ArrayList<BiometricDTO> babylist2(BiometricDTO bd);
+	
+	public ArrayList<BiometricDTO> babylist3(BiometricDTO bd);
 	
 	//입원
 	public int baby_room_in(BiometricDTO bd);
@@ -22,13 +26,19 @@ public interface BiometricService {
 	//아기 입원 테이블 리스트 중에 값 확인
 	public BiometricDTO baby_room_sel(BiometricDTO bd);
 	
-	//아기 입원 테이블 리스트
+	//아기 등록창
+	public ArrayList<BiometricDTO> bt301(BiometricDTO bd);
+	
 	public ArrayList<BiometricDTO> bt302(BiometricDTO bd);
+	
+	public ArrayList<BiometricDTO> bt303(BiometricDTO bd);
 	
 	//퇴원
 	public int baby_room_del(BiometricDTO bd);
 	
 	//중복 검사
 	public int baby_room_Overlapping(BiometricDTO bd);
+
+	
 
 }
