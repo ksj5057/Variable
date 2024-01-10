078a904475c954f27df65d8dd1236e8f7011b4d0
worntjd142 <149766907+worntjd142@users.noreply.github.com>
2024-01-10 17:46:51 +0900

    123

--- src/main/java/com/vr/Controller/TemperatureController.java
+++ src/main/java/com/vr/Controller/TemperatureController.java
@@ -20,29 +20,49 @@ public class TemperatureController {
 
 	@GetMapping("/get/room/tem/{bt}/{tem}")
 	public ResponseEntity<?> controller(@PathVariable int bt, @PathVariable int tem, TemperatureDTO td, Model model){
-		System.out.println(bt);
+		//카운터 초기화
 		int count = 0;
+		
+		// bt의 값이 1이라면
 		if(bt == 1) {
+			// DTO의 bt(구분번호), tem(온도)저장
 			td.setBt(bt);
 			td.setTem(tem);
+			
+			//db의 301호실의 온도 값을 저장하면 count에 값을 저장 1= 등록 0=실패
 			count =	ti.bt301_tem(td);
+		
+		// bt의 값이 2이라면
 		}else if(bt == 2) {
+			// DTO의 bt(구분번호), tem(온도)저장
 			td.setBt(bt);
 			td.setTem(tem);
+			
+			//db의 302호실의 온도 값을 저장하면 count에 값을 저장 1= 등록 0=실패
 			count = ti.bt302_tem(td);
 			
+			
+		// bt의 값이 3이라면	
+			// DTO의 bt(구분번호), tem(온도)저장
 		}else if(bt == 3) {
 			td.setBt(bt);
 			td.setTem(tem);
+			
+			//db의 303호실의 온도 값을 저장하면 count에 값을 저장 1= 등록 0=실패
 			count =	ti.bt303_tem(td);
 			
+			
+		// bt의 값이 4이라면	
+			// DTO의 bt(구분번호), tem(온도)저장
 		}else if(bt == 4) {
 			td.setBt(bt);
 			td.setTem(tem);
+			
+			//db의 모든 호실의 온도 값을 저장하면 count에 값을 저장 1= 등록 0=실패
 			count =ti.batches_tem(td);
 		}
 		
-
+				//카운터의 값이 0 or 1을 반환
 		return new ResponseEntity<>((count),HttpStatus.OK);
 	
 	}
