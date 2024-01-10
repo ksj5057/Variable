bcf43a234cfb95865368248c7dc6794f0ff4c918
GR <GR@GRN-304-05>
2024-01-10 14:45:25 +0900

    # WARNING: head commit changed in the meantime
    Merge branch 'eob' of https://github.com/ksj5057/Variable.git into eob
    
    Conflicts:
    	target/classes/com/vr/Controller/TempleController.class
    	target/classes/com/vr/Model/TempleDAO$AjaxDAO.class

--- src/main/webapp/resources/js/biometric/test.js
+++ src/main/webapp/resources/js/biometric/test.js
@@ -1,49 +1,65 @@
-let today = Array(24); //날짜
-let temp = Array(24);    //온도
-let i=0;				
-$(document).ready(function test(){
-		
-	$.getJSON("/post/chart/time.json", function (data){
-		
-		
-		today[i] = data[0].today;
-		console.log(today)
-		temp[i] = data[0].temp;
-		console.log(temp)
-		i++;
 
-			//chart(data);
-	})
+/* 실시간 온도 체크 그래프 */
 
-		
-	new Chart(document.getElementById("line-chart"), {
-		  type: 'line',
-		  data: {
-		    // X축
-			labels: [today[0]],
-		    datasets: [{
-		    	// DB데이터 y 축 그래프 기준 온도
-		        data: [24, 25, 26, 27, 28, 29, 30, 31,32,33,34,35,36,37,38,39,40],
-		        fill: false
-		      }, 
-		      { 
-		          data: [" ", temp[0]],
-		          label: "김륵싼",
-		          borderColor: "#8e5ea2",
-		          fill: false
-		        }
-		    ]
-		  },
-		  options: {
-		    title: {
-		      display: true,
-		      text: '실시간 온도 그래프'
-		    }
-		  }
-		});
+$(function(){
 
-	setInterval(test, 20000);
+	// 그래프 차트
+	var ctx_live = document.getElementById("mycanvas");
+	var myChart = new Chart(ctx_live, {
+		        
+	type: 'line',
+	  data: {
+	    labels: [],
+	    datasets: [{
+	      data: [],
+	      borderWidth: 1,
+	      borderColor:'#00c0ef',
+	      label: 'liveCount',
+	    }]
+	  },
+	  options: {
+	    responsive: true,
+	    title: {
+	      display: true,
+	      text: "Chart.js - Dynamically Update Chart Via Ajax Requests",
+	    },
+	    legend: {
+	      display: false
+	    },
+	    scales: {
+	      yAxes: [{
+	        ticks: {
+	          beginAtZero: true,
+	        }
+	      }]
+	    }
+	  }
+	});
 
-})
+	// 새로운 데이터를 data에 저장 출력
+	var getData = function() {
+	  $.ajax({
+	    url: '/test.action',
+	    dataType: 'json',
+	    type: 'get',
+	    async : false,
+	    success: function(data) {
+	      // process your data to pull out what you plan to use to update the chart
+	      // e.g. new label and a new data point
+	      
+	      // add new label and data point to chart's underlying data structures
+	      myChart.data.labels.push(data[0].today);
+	      myChart.data.datasets[0].data.push(data[0].temp);
+	      console.log(data[0].temp);
+	      console.log(data[0].today);
+	      
+	      // re-render the chart
+	      myChart.update();
+	    }
+	  });
+	};
 
+	// get new data every 3 seconds
+	setInterval(getData, 60000);
 
+});
